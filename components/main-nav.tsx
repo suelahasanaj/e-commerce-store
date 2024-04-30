c78935ed1303f1b14a5d83f9fa8c58fcf0a37702
suelahasanaj <suelahasanaj6@gmail.com>
2024-04-30 10:37:09 +0200

    Second part of project (Store) - Environment setup and organized structure

---
+++ components/main-nav.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+interface MainNavProps {
+  data: any;
+}
+
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  // const pathname=usePathname();
+
+  // const routes=data.map((route)=>({
+  //     href: `/category/${route.id}`,
+  //     label: route.name,
+  //     active: pathname===`/category/${route.id}`
+  // }));
+
+  return <nav></nav>;
+};
+export default MainNav;
