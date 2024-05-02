206cd9f779dc22976273c0cab24a97db44e63ff6
suelahasanaj <suelahasanaj6@gmail.com>
2024-05-02 11:03:55 +0200

    Added individual product screen that contains images of the product and info like name,price,size,color and suggested items

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -14,14 +14,14 @@ const getProducts = async (query: Query): Promise<Product[]> => {
   const url = qs.stringifyUrl({
     url: URL,
     query: {
-      categoryId: query.categoryId,
-      sizeId: query.sizeId,
       colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
       isFeatured: query.isFeatured,
     },
   });
 
-  const res = await fetch(URL);
+  const res = await fetch(url);
   return res.json();
 };
 export default getProducts;
